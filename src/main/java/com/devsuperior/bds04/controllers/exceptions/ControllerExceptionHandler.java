676739e471fb6006890bfcb8bf34ddaa98e6e6fb
arthurtavora <arthurtavorademello@gmail.com>
2021-10-08 15:47:40 -0300

    Create custom validation response (MethodArgumentNotValidException handling)

--- src/main/java/com/devsuperior/bds04/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/bds04/controllers/exceptions/ControllerExceptionHandler.java
@@ -40,4 +40,21 @@ public class ControllerExceptionHandler {
 		err.setPath(request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(status).body(err);
+	}
 }
