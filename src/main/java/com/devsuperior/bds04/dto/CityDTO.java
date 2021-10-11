c03a3d3c77a2c7b77972ffc6a172fe1d0239adb1
arthurtavora <arthurtavorademello@gmail.com>
2021-10-11 19:38:37 -0300

    Adjust final configurations. Events were not being displayed when getting cities. Everything working fine now

--- src/main/java/com/devsuperior/bds04/dto/CityDTO.java
+++ src/main/java/com/devsuperior/bds04/dto/CityDTO.java
@@ -3,10 +3,12 @@ package com.devsuperior.bds04.dto;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import javax.validation.constraints.NotBlank;
 
 import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.entities.Event;
 
 public class CityDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -29,6 +31,12 @@ public class CityDTO implements Serializable {
 	public CityDTO(City entity) {
 		id = entity.getId();
 		name = entity.getName();
+		entity.getEvents().forEach(event -> events.add(new EventDTO(event)));
+	}
+	
+	public CityDTO(City entity, Set<Event> events) {
+		this(entity);
+		events.forEach(event -> this.events.add(new EventDTO(event)));
 	}
 
 	public Long getId() {
